463795a26dfcd9b12c5a2ed2da70f9f7a4a446e4
Andrey Marin <a.marin@modulbank.ru>
2023-12-26 18:37:04 +0100

    formatted time

--- app/src/main/java/com/example/mystatisticapp/TimerApp.kt
+++ app/src/main/java/com/example/mystatisticapp/TimerApp.kt
@@ -1,5 +1,3 @@
-
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,6 +16,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -50,9 +49,21 @@ fun HomeScreen(db: AppDatabase) {
         val categories = categoriesFlow.collectAsState(initial = listOf()).value
         val categoriesNames: List<String> = categories.map { it.name }
 
+
+        val formattedTime = if (isRunning) {
+            val hours = currentTime / 3600
+            val minutes = (currentTime % 3600) / 60
+            val seconds = currentTime % 60
+
+            "%02d:%02d:%02d".format(hours, minutes, seconds)
+        } else {
+            ""
+        }
+
         Text(
-            text = if (isRunning) "$currentTime" else "",
-            style = MaterialTheme.typography.h6,
+            fontFamily = FontFamily.Monospace,
+            text = formattedTime,
+            style = MaterialTheme.typography.h2,
             modifier = Modifier.padding(16.dp)
         )
 
@@ -96,25 +107,28 @@ fun HomeScreen(db: AppDatabase) {
                 onConfirmRequest = {
                     showDialog = false
 
-                    if (!createdCategory.isNullOrBlank()){
-                        val newTimer = TimerData(category = createdCategory, timeInSeconds = currentTime)
+                    if (!createdCategory.isNullOrBlank()) {
+                        val newTimer =
+                            TimerData(category = createdCategory, timeInSeconds = currentTime)
                         coroutineScope.launch {
                             db.categoryDao().insertCategory(Category(name = createdCategory))
                             db.timerDao().insertTimer(newTimer)
                         }
 
-                        Log.d("MY_TAG", "HomeScreen: createdCategory = $createdCategory")
                     } else if (selectedCategory.value.isNotEmpty()) {
                         // Если выбрана категория, сохраните данные в базу данных
-                        val newTimer = TimerData(category = selectedCategory.value, timeInSeconds = currentTime)
+                        val newTimer = TimerData(
+                            category = selectedCategory.value,
+                            timeInSeconds = currentTime
+                        )
                         coroutineScope.launch {
                             db.timerDao().insertTimer(newTimer)
                         }
                     }
                 },
                 onDismissRequest = {
-                showDialog = false
-            }, onCategorySelected = {
+                    showDialog = false
+                }, onCategorySelected = {
                     selectedCategory.value = it
                 },
                 onCategoryCreated = {
