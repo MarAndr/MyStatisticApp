0af837745060b616f2e084b0b03c16458992b9de
Andrey Marin <a.marin@modulbank.ru>
2024-01-03 14:26:22 +0100

    put fonts montserrat and used it

--- app/src/main/java/com/tracker/trackDailyHub/StartScreen.kt
+++ app/src/main/java/com/tracker/trackDailyHub/StartScreen.kt
@@ -5,12 +5,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -27,15 +27,14 @@ fun StartScreen(
         verticalArrangement = Arrangement.Center
     ) {
 
-        androidx.compose.material.Text(
-            fontFamily = FontFamily.Monospace,
+        Text(
             text = time,
-            style = MaterialTheme.typography.h2,
+            style = MaterialTheme.typography.h1,
             modifier = Modifier.padding(16.dp)
         )
 
         if (!isRunning) {
-            androidx.compose.material.Button(
+            Button(
                 onClick = { onStartButtonClick() },
             ) {
                 Text(text = "Start", style = MaterialTheme.typography.h1)
@@ -48,11 +47,11 @@ fun StartScreen(
                 androidx.compose.material.Button(
                     onClick = { onStopButtonClick() }
                 ) {
-                    androidx.compose.material.Text(text = "Stop")
+                    androidx.compose.material.Text(text = "Stop", style = MaterialTheme.typography.h1)
                 }
 
                 androidx.compose.material.Button(onClick = { onPauseButtonClick() }) {
-                    androidx.compose.material.Text(text = "Pause")
+                    androidx.compose.material.Text(text = "Pause", style = MaterialTheme.typography.h1)
                 }
             }
         }
--- app/src/main/java/com/tracker/trackDailyHub/ui/theme/Type.kt
+++ app/src/main/java/com/tracker/trackDailyHub/ui/theme/Type.kt
@@ -2,28 +2,31 @@ package com.tracker.trackDailyHub.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.tracker.trackdailyhub.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    defaultFontFamily = FontFamily(
+        Font(R.font.montserrat_regular, FontWeight.Normal),
+        Font(R.font.montserrat_bold, FontWeight.Bold)
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     h1 = TextStyle(
-        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     h2 = TextStyle(
-        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 16.sp,
