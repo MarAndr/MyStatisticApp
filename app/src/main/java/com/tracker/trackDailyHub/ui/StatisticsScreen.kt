44f9eba18dc4193e0bad8fb82058803e1f0fd108
Andrey Marin <a.marin@modulbank.ru>
2024-01-04 15:59:08 +0100

    added charts library

--- app/src/main/java/com/tracker/trackDailyHub/ui/StatisticsScreen.kt
+++ app/src/main/java/com/tracker/trackDailyHub/ui/StatisticsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,6 +14,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import co.yml.charts.axis.AxisData
+import co.yml.charts.common.extensions.formatToSinglePrecision
+import co.yml.charts.common.model.Point
+import co.yml.charts.ui.linechart.LineChart
+import co.yml.charts.ui.linechart.model.GridLines
+import co.yml.charts.ui.linechart.model.IntersectionPoint
+import co.yml.charts.ui.linechart.model.Line
+import co.yml.charts.ui.linechart.model.LineChartData
+import co.yml.charts.ui.linechart.model.LinePlotData
+import co.yml.charts.ui.linechart.model.LineStyle
+import co.yml.charts.ui.linechart.model.SelectionHighlightPoint
+import co.yml.charts.ui.linechart.model.SelectionHighlightPopUp
+import co.yml.charts.ui.linechart.model.ShadowUnderLine
 import com.tracker.trackDailyHub.database.AppDatabase
 
 
@@ -28,6 +42,59 @@ fun StatisticScreen(db: AppDatabase) {
     }
 
     SectionList(sections = categoryToTotalTime)
+//    SingleLineChartWithGridLines(
+//        DataUtils.getLineChartData(
+//            100,
+//            start = 50,
+//            maxRange = 100
+//        )
+//    )
+}
+
+@Composable
+fun SingleLineChartWithGridLines(pointsData: List<Point>) {
+    val steps = 5
+    val xAxisData = AxisData.Builder()
+        .axisStepSize(30.dp)
+        .topPadding(105.dp)
+        .steps(pointsData.size - 1)
+        .labelData { i -> pointsData[i].x.toInt().toString() }
+        .labelAndAxisLinePadding(15.dp)
+        .build()
+    val yAxisData = AxisData.Builder()
+        .steps(steps)
+        .labelAndAxisLinePadding(20.dp)
+        .labelData { i ->
+            // Add yMin to get the negative axis values to the scale
+            val yMin = pointsData.minOf { it.y }
+            val yMax = pointsData.maxOf { it.y }
+            val yScale = (yMax - yMin) / steps
+            ((i * yScale) + yMin).formatToSinglePrecision()
+        }.build()
+    val data = LineChartData(
+        linePlotData = LinePlotData(
+            lines = listOf(
+                Line(
+                    dataPoints = pointsData,
+                    LineStyle(),
+                    IntersectionPoint(),
+                    SelectionHighlightPoint(),
+                    ShadowUnderLine(),
+                    SelectionHighlightPopUp()
+                )
+            )
+        ),
+        xAxisData = xAxisData,
+        yAxisData = yAxisData,
+        gridLines = GridLines()
+    )
+    LineChart(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp),
+        lineChartData = data
+    )
+
 }
 
 
